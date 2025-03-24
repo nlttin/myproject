5629f41dbd9763e84b11fc01cc7b96a26c775e3f
nlttin <55130844+nlttin@users.noreply.github.com>
2025-03-24 16:43:12 +0700

    new1

--- frontend/src/components/MovieList.js
+++ frontend/src/components/MovieList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Col, Row, Container } from 'react-bootstrap';
+import { Card, Col, Row, Container, CardBody, CardTitle, CardText } from 'react-bootstrap';
 
 function MovieList() {
     const [movies, setMovies] = useState([]);
@@ -10,7 +10,6 @@ function MovieList() {
             .then(data => setMovies(data))
             .catch(error => console.error('Lỗi:', error));
     }, []);
-
     return (
         <Container>
             <h1 className="mt-4">Danh sách phim</h1>
@@ -18,12 +17,12 @@ function MovieList() {
                 {movies.map(movie => (
                     <Col md={4} key={movie._id} className="mb-4">
                         <Card>
-                            <Card.Img variant="top" src={`/media/${movie._id}.jpg`} alt={movie.title} />
-                            <Card.Body>
-                                <Card.Title>{movie.title}</Card.Title>
-                                <Card.Text>{movie.description}</Card.Text>
-                                <a href={`/movies/${movie._id}`} className="btn btn-primary">Xem</a>
-                            </Card.Body>
+                            <Card.Img variant="top" src={ `/media/${movie._id}.jpg` } alt={movie.title} />
+                            <CardBody>
+                                <CardTitle>{movie.title}</CardTitle>
+                                <CardText>{movie.description}</CardText>
+                                <a href={ `/movies/${movie._id}` } className="btn btn-primary">Xem</a>
+                            </CardBody>
                         </Card>
                     </Col>
                 ))}
